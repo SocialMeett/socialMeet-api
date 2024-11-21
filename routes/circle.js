f76b678c44bd17c62ea9d655ced0172c98e99a4d
Jacqueline Offei <offeijacqueline@gmail.com>
2024-11-21 14:19:12 +0000

    removing id in circle route

--- routes/circle.js
+++ routes/circle.js
@@ -10,7 +10,7 @@ circleRouter.post("/circles", isAuthenticated,createCircle);
 // delete circle
 circleRouter.delete("/circles/:id", isAuthenticated,deleteCircle);
 // join circle
-circleRouter.post("/circles/:id/join",isAuthenticated,joinCircle);
+circleRouter.post("/circles/join",isAuthenticated,joinCircle);
 
 // get a circle by id
 circleRouter.get('/circles/:id', isAuthenticated, getCircleById  )
